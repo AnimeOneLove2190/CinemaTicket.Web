0c2942ec6d5ffd2deab33296e22bafd183e3188a
animeonelove2190 <daniil.davletshin1@gmail.com>
2023-11-23 03:50:16 +0500

    feature/611/1 Add NLog.web Libbrary Add NLog.config Changed configuration in the Program.cs

--- CinemaTicket.WebApi/Program.cs
+++ CinemaTicket.WebApi/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog.Web;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,7 +22,12 @@ namespace CinemaTicket.WebApi
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                }).ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(LogLevel.Trace);
+                })
+                .UseNLog();
         //Add develop branch
         //Add feature/602
         //Add feature/604
