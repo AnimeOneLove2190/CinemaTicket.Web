009a78ff722f471e8d2396dbf74b76649a353793
animeonelove2190 <daniil.davletshin1@gmail.com>
2023-11-26 21:46:36 +0500

    feature/615 WeatherForecastController removed Removed class WeatherForecast All endpoints except GetInfo have been removed from the HomeController controller

--- CinemaTicket.WebApi/Controllers/HomeController.cs
+++ CinemaTicket.WebApi/Controllers/HomeController.cs
@@ -12,47 +12,11 @@ namespace CinemaTicket.WebApi.Controllers
     [ApiController]
     public class HomeController : ControllerBase
     {
-        private static List<TestObject> testObjects = new List<TestObject>();
         [HttpGet]
         [Route("info")]
         public string GetInfo()
         {
             return $"Backend is started {DateTime.Now}";
-        }
-        [HttpPost]
-        [Route("Hello")]
-        public string AddName(string name)
-        {
-            return $"Hello, {name}!";
-        }
-        [HttpGet]
-        [Route("GetTestObject")]
-        public TestObject GetTestObject()
-        {
-            var testObject = new TestObject
-            {
-                Name = "Misato",
-                Description = "Commanger Of Evangelion Squad",
-                CreateDate = new DateTime(2023, 10, 30, 20, 46, 00)
-            };
-            return testObject;
-        }
-        [HttpGet]
-        [Route("GetTestObjects")]
-        public List<TestObject> GetTestObjects()
-        {
-            return testObjects;
-        }
-        [HttpPost]
-        [Route("AddTestObject")]
-        public bool PostTestObject(TestObject testObject)
-        {
-            if (testObject == null)
-            {
-                return false;
-            }
-            testObjects.Add(testObject);
-            return true;
         }
     }
 }
