14b27af1c63f2384b32ca12fe184da64627b9b51
animeonelove2190 <daniil.davletshin1@gmail.com>
2023-12-14 21:20:15 +0500

    Edits made

--- CinemaTicket.Infrastructure/Constants/BulkTicketCreateTemplate.cs
+++ CinemaTicket.Infrastructure/Constants/BulkTicketCreateTemplate.cs
@@ -6,7 +6,7 @@ namespace CinemaTicket.Infrastructure.Constants
 {
     public static class BulkTicketCreateTemplate
     {
-        public const string Name = "Bulk Ticket Create Template.xlxs";
+        public const string Name = "Bulk Ticket Create Template.xlsx";
         public const string WorksheetName = "Tickets";
         public static readonly List<string> ColumnNames = new List<string> { Column.RowNumber, Column.PlaceNumber, Column.Price };
         public static class Column
